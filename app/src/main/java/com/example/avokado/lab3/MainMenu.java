2b6196ab2f2b195dc6b8c98404a65a71bb2eeff7
Aksel Hjerpbakk <akselhj@stud.ntnu.no>
2018-05-05 17:06:32 +0200

    test

--- app/src/main/java/com/example/avokado/lab3/MainMenu.java
+++ app/src/main/java/com/example/avokado/lab3/MainMenu.java
@@ -1,10 +1,20 @@
 package com.example.avokado.lab3;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+
+import static com.google.android.gms.common.GooglePlayServicesUtil.isGooglePlayServicesAvailable;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -27,6 +37,15 @@ public class MainMenu extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 				Intent gameIntent = new Intent(MainMenu.this, LocalBoardActivity.class);
+				startActivity(gameIntent);
+			}
+		});
+
+		// Global leaderboard Option - Brings up a global leaderboard up
+		findViewById(R.id.mm_glolead_bt).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent gameIntent = new Intent(MainMenu.this, GlobalBoardActivity.class);
 				startActivity(gameIntent);
 			}
 		});
